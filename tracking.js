dd4bdd2323e5f021f087bf9bab143f8778fa59ef
Serhii Kolodych <kolodych@hotmail.com>
2025-04-26 13:14:25 +0300

    update

--- tracking.js
+++ tracking.js
@@ -1,49 +1,32 @@
 console.log("🐞 start loading scripts... 🐞");
 
-// Array to keep track of loaded scripts
-var loadedScripts = [];
+// Google Analytics
+(function () {
+  var gaScript = document.createElement("script");
+  gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-N496N6P80K";
+  gaScript.async = true;
+  document.head.appendChild(gaScript);
 
-// Define gtag globally
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-
-// Function to load external scripts dynamically and track them
-function loadScript(src, async = true) {
-  var script = document.createElement("script");
-  script.src = src;
-  script.async = async;
-  document.head.appendChild(script);
-
-  loadedScripts.push(script);
-}
-
-// Function to load the tracking scripts
-function loadTrackingScripts() {
-  // Google Analytics
-  loadScript("https://www.googletagmanager.com/gtag/js?id=G-N496N6P80K");
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
   gtag("js", new Date());
   gtag("config", "G-N496N6P80K");
+})();
 
-  // Microsoft Clarity
-  (function (c, l, a, r, i, t, y) {
-    c[a] =
-      c[a] ||
-      function () {
-        (c[a].q = c[a].q || []).push(arguments);
-      };
-    t = l.createElement(r);
-    t.async = 1;
-    t.src = "https://www.clarity.ms/tag/" + i;
-    y = l.getElementsByTagName(r)[0];
-    y.parentNode.insertBefore(t, y);
-
-    loadedScripts.push(t);
-  })(window, document, "clarity", "script", "nar7r3ykv5");
-}
-
-// Expose the function globally
-window.loadTrackingScripts = loadTrackingScripts;
+// Microsoft Clarity
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "nar7r3ykv5");
 
 console.log("🪲 scripts loaded successfully! 🪲");
